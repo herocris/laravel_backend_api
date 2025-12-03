20f805e795d3b50a9880dd3c166fdf1db8e6f919
Cristhian Torres <cris_itg@yahoo.es>
2025-12-03 14:09:16 +0100

    Creación de usuario administrador para testing de features

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use DatabaseTransactions;
+
+    public function superAdminLogin(): User
+    {
+        return User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@yahoo.es',
+            'password' => bcrypt('password'),
+        ]);
+    }
 }
